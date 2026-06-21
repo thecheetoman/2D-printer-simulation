3ea7c24eaed03949614350d26f8d6e541835b1d7
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-06-21 14:44:29 -0700

    more parser stuff

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,8 @@
 import sys
 from pathlib import Path
+from printer import Printer
+
+printer = Printer()
 
 # Check if the .banana file was ever included
 if len(sys.argv) < 2:
@@ -15,6 +18,37 @@ print(target_file)
 try:
     with open(target_file, "r") as sliceimage:
         contents = sliceimage.readlines()
-        print(contents)
+        print("Raw file contents " + str(contents))
+        print("\n \n Processing PT 1(Fixing spacing)")
+        
+        commands = []
+
+        # .banana processing
+        for line in contents:
+            #Strip line of unnecesary styff
+            line = line.strip()
+            #Null check
+            if not line:
+                continue
+            #Process each command and seperate each value from text
+            #EX MOVE 50 50 ["MOVE", "50", "50"]
+            token = line.split()
+
+            #Token processing
+            if token[0] == "HOME":
+                commands.append(("HOME",))
+            elif token[0] == "MOVE":
+                commands.append(("MOVE", int(token[1]), int(token[2])))
+            elif token[0] == "START":
+                commands.append(("START",))
+            elif token[0] == "END":
+                commands.append(("END"),)
+            elif token[0] == "COLOR":
+                commands.append(("COLOR", int(token[1]), int(token[2]), int(token[3])))
+            #Print out each line
+            print(token)
+        print(commands)
+        for command in commands:
+            printer.execute(command)
 except FileNotFoundError:
     print("Error: The .banana file does not exist")
---
+++ src/printer.py
@@ -0,0 +1,35 @@
+class Printer:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.r = 0
+        self.g = 0
+        self.b = 0
+        self.pen_down = False
+
+    def execute(self, command):
+        if command[0] == "HOME":
+            self.x = 0
+            self.y = 0
+
+        elif command[0] == "MOVE":
+            if len(command) >= 3:
+                self.x = command[1]
+                self.y = command[2]
+            else:
+                print(f"Error: Invalid MOVE command format: {command}")
+
+        elif command[0] == "START":
+            self.pen_down = True
+
+        elif command[0] == "END":
+            self.pen_down = False
+        elif command[0] == "COLOR":
+            if len(command) >= 4:
+                self.r = command[1]
+                self.g = command[2]
+                self.b = command[3]
+            else:
+                print(f"Error: Invalid COLOR command format: {command}")
+
+        print(f"Position: ({self.x}, {self.y})")
