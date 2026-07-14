09fcead2b606119ab35f49450bd2dca28cf502eb
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-07-14 14:22:35 -0700

    added fill info and stroke(border) info to the shape extractor. refactored its code a bit

--- src/slicer/main.py
+++ src/slicer/main.py
@@ -24,17 +24,45 @@ def dumpSvg(target):
 def extractShapes(target):
     targetSvgInstance = svg.SVG.parse(target)
     for element in targetSvgInstance.elements():
-        # check if the image has a rectangle
-        if isinstance(element, svg.Rect):
-            print("\nrectangle present:")
-            print(f"position: x={element.x}, y={element.y}")
-            print(f"dimensions: width={element.width}, height={element.height}")
+        #check if the shape is a rectangle or circle
+        isRect = isinstance(element, svg.Rect)
+        isCircle = isinstance(element, svg.Circle)
+
+        if isRect or isCircle:
+
+            fillObject = element.fill
+            #check if the fill value is not present, otherwise, put it into fillInfo string
+            if fillObject is None or fillObject == "none" or fillObject.opacity == 0:
+                fillInfo = "transparent or something"
+            else:
+                fillInfo = "infill: " + str(fillObject.hex) + "(printer doesnt support opacity, so it will ignore it)"
+
+            #similar check to see if there is a stroke(border)
+            strokeObject = element.stroke
+            strokeWidthPreProcess = element.stroke_width
+
+            if strokeObject is None or strokeObject == "none" or strokeWidthPreProcess == 0:
+                strokeInfo = "no stroke"
+            else:
+                strokeInfo = "stroke color: " + str(strokeObject.hex) + "stroke width: " + str(strokeWidthPreProcess) + "px"
             
-        # check if there is a circle
-        elif isinstance(element, svg.Circle):
-            print("\ncircle present")
-            print(f"center: cx={element.cx}, cy={element.cy}")
-            print(f"radius: r={element.r}")
+            # printing the information
+            #                
+            # check if the image has a rectangle
+            if isinstance(element, svg.Rect):
+                print("\nrectangle present:")
+                print(f"position: x={element.x}, y={element.y}")
+                print(f"dimensions: width={element.width}, height={element.height}")
+                print(fillInfo)
+                print(strokeInfo)
+
+            # check if there is a circle
+            elif isinstance(element, svg.Circle):
+                print("\ncircle present")
+                print(f"center: cx={element.cx}, cy={element.cy}")
+                print(f"radius: r={element.rx}")
+                print(fillInfo)
+                print(strokeInfo)
 
 dumpSvg(target_svg)
 extractShapes(target_svg)
