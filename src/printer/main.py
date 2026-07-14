61a697a3554fbb84111ab667b2e1fd497de15963
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-07-14 12:12:11 -0700

    fix some paths on printer. basic SVG dumper

--- src/printer/main.py
+++ src/printer/main.py
@@ -27,7 +27,7 @@ NOZZLE_SCALE = 1.0
 NOZZLE_TIP_X = 64
 NOZZLE_TIP_Y = 126
 nozzle_img = None
-nozzle_path = os.path.join(os.path.dirname(__file__), "../../assets/nozzle.png")
+nozzle_path = os.path.join(os.path.dirname(__file__), "../..    /assets/nozzle.png")
 try:
     nozzle_img = pygame.image.load(nozzle_path).convert_alpha()
     if NOZZLE_SCALE != 1.0:
--- src/slicer/main.py
+++ src/slicer/main.py
@@ -1,1 +1,22 @@
-# Placeholder
+import svgelements as svg
+import sys 
+
+#check it user provided a target svg
+if len(sys.argv) < 2:
+    print("Please provide a valid target to an SVG")
+    print("EX: python3 main.py testsubject.svg")
+    sys.exit()
+
+#check if user provided actually provided an svg or a random file
+target_svg = sys.argv[1]
+if ".svg" not in target_svg:
+    print("Please provide a valid target to an SVG")
+    print("EX: python3 main.py testsubject.svg")
+    sys.exit()
+
+def dumpSvg(target):
+    targetSvgInstance = svg.SVG.parse(target)
+    stringDump = targetSvgInstance.string_xml()
+    print(stringDump)
+
+dumpSvg(target_svg)
